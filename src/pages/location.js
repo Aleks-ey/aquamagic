4cb0049a320912fc65b6d1797d9be5a70ef39cb7
Aleksey Huhua <aleksey@Alekseys-MacBook-Pro.local>
2023-10-04 23:27:32 -0600

    iframe sizing 3

--- src/pages/location.js
+++ src/pages/location.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 const Location = () => {
-    if(document.getWindowWidth < 768) {
+    if(useWindowSize < 768) {
         document.getElementById("maps").className = "phone_maps";
     }
     else {
@@ -66,5 +66,36 @@ const Location = () => {
         
     );
 }
+
+function useWindowSize() {
+    // Initialize state with undefined width/height so server and client renders match
+    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+    const [windowSize, setWindowSize] = useState({
+      width: undefined,
+      height: undefined,
+    });
+  
+    useEffect(() => {
+      // only execute all the code below in client side
+      // Handler to call on window resize
+      function handleResize() {
+        // Set window width/height to state
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+      
+      // Add event listener
+      window.addEventListener("resize", handleResize);
+       
+      // Call handler right away so state gets updated with initial window size
+      handleResize();
+      
+      // Remove event listener on cleanup
+      return () => window.removeEventListener("resize", handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
+    return windowSize;
+  }
  
 export default Location;
