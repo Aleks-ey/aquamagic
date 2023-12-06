46234f270f72c2f1dd92d9f65ee2fd50370c5b63
Aleks-ey <Akhukhua@yahoo.com>
2023-12-06 13:13:56 -0700

    calendar css file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
 import '@/styles/event.css'
+import '@/styles/calendar.css'
+
 import Layout from '@/components/layout'
 
 export default function App({ Component, pageProps }) {
--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -129,12 +129,3 @@ function formatStandardTime(timeString) {
 }
  
 export default Schedule;
-
-// return (
-//     <div className="flex mb-32 bg-white min-h-screen mt-12 md:pt-20 justify-center">
-//         <div className="flex bg-gradient-to-br from-pool-water to-white w-11/12 h-auto rounded-md">
-//             <Image src="/ScheduleOct23.png" width="1000" height="1000" className="hidden md:block w-full h-full rounded-md"/>
-//             <Image src="/ScheduleOct23.png" width="1000" height="200" className="md:hidden w-full rounded-md"/>
-//         </div>
-//     </div>
-// );
